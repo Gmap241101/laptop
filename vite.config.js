285f6ce627ec0ff6e22c89d0282c3fbd537b7f17
Gmap241101 <gm.ap.241101@recruit.dooray.com>
2026-07-23 16:57:54 +0900

    운영 배포_20260723_165745

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { initializeApp } from 'firebase/app' // 안전장치용 임포트 무시
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "./", // 깃허브 저장소 이름인 'laptop' 주소를 올바르게 인식
+  base: "/", // 커스텀 도메인의 루트 경로를 기준으로 정적 파일 로드
   plugins: [react()],
 })
